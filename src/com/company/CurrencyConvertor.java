a6ea8a0c9e5ca6841f760e4a9491060a7ca35bd1
Igor755 <metlin.igor@gmail.com>
2018-06-10 23:00:45 +0300

    ready

--- src/com/company/CurrencyConvertor.java
+++ src/com/company/CurrencyConvertor.java
@@ -10,36 +10,33 @@ import java.util.Scanner;
 
 import com.google.gson.Gson;
 
-/**
- *
- * @author Dinesh Krishnan
- *
- */
+
 
 public class CurrencyConvertor {
 
     // API Provider URL http://data.fixer.io/api/latest?access_key=395a99dddbab6370cd145f2534fbc113&usd
     private static final String API_PROVDIER = "http://data.fixer.io/api/";
+    private static final String KEY = "395a99dddbab6370cd145f2534fbc113&";
 
     public static double convert(String fromCurrencyCode, String toCurrencyCode) {
 
         if ((fromCurrencyCode != null && !fromCurrencyCode.isEmpty())
                 && (toCurrencyCode != null && !toCurrencyCode.isEmpty())) {
 
-            CurrencyConversionResponse response = getResponse(API_PROVDIER+"latest?access_key=395a99dddbab6370cd145f2534fbc113&"+fromCurrencyCode);
+            CurrencyConversionResponse response = getResponse(API_PROVDIER + "latest?access_key="+ KEY + fromCurrencyCode);
 
-            if(response != null) {
+            if (response != null) {
 
                 String rate = response.getRates().get(toCurrencyCode);
 
-                double conversionRate = Double.valueOf((rate != null)?rate:"0.0");
+                double conversionRate = Double.valueOf((rate != null) ? rate : "0.00");
 
                 return conversionRate;
             }
 
         }
 
-        return 0.0;
+        return 0.00;
     }
 
     // Method to get the response from API
@@ -50,7 +47,7 @@ public class CurrencyConvertor {
         Gson gson = new Gson();
         StringBuffer sb = new StringBuffer();
 
-        if(strUrl == null || strUrl.isEmpty()) {
+        if (strUrl == null || strUrl.isEmpty()) {
 
             System.out.println("Application Error");
             return null;
@@ -97,23 +94,40 @@ public class CurrencyConvertor {
         Scanner scanner = new Scanner(System.in);
 
 
-        String fromCurrencyCode  = "USD";
+        String fromCurrencyCode = "USD";
 
         System.out.println("Enter the price :");
         double amount = scanner.nextDouble();
 
-        System.out.println("Enter the Currency Code that you want to covert : ");
+        System.out.println("Enter the Currency Code that you want to covert: UAH, EUR, GBP");
         String toCurrencyCode = scanner.next();
-
         fromCurrencyCode = fromCurrencyCode.toUpperCase();
         toCurrencyCode = toCurrencyCode.toUpperCase();
 
-        // Currency Conversion
-        double coversionRate = convert(fromCurrencyCode, toCurrencyCode);
 
-        System.out.println("Hi, The "+amount +" "+ fromCurrencyCode+" is equivalent to "+(coversionRate * amount)+" "+toCurrencyCode+" today.");
+        if (toCurrencyCode.equals("UAH")) {
+            double coversionRate = convert(fromCurrencyCode, toCurrencyCode);
 
-        scanner.close();
+            System.out.println("Hi, The " + amount + " " + fromCurrencyCode + " is equivalent to " + (coversionRate * amount) + " " + toCurrencyCode + " today.");
+            scanner.close();
+
+
+        } else if (toCurrencyCode.equals("EUR")) {
+            double coversionRate = convert(fromCurrencyCode, toCurrencyCode);
+
+            System.out.println("Hi, The " + amount + " " + fromCurrencyCode + " is equivalent to " + (coversionRate * amount) + " " + toCurrencyCode + " today.");
+            scanner.close();
+
+        } else if (toCurrencyCode.equals("GBP")) {
+            double coversionRate = convert(fromCurrencyCode, toCurrencyCode);
+
+            System.out.println("Hi, The " + amount + " " + fromCurrencyCode + " is equivalent to " + (coversionRate * amount) + " " + toCurrencyCode + " today.");
+            scanner.close();
+
+        } else {
+            System.out.println("Incorected Currency code");
+        }
+
     }
 
 }
